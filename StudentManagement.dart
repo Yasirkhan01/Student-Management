b6aebfdcd793557e7542b2b3b903f17ba41866db
Yasir Khan <78487913+Yasirkhan01@users.noreply.github.com>
2023-07-18 21:22:03 +0500

    Update StudentManagement.dart

--- StudentManagement.dart
+++ StudentManagement.dart
@@ -1,6 +1,52 @@
 import 'dart:io';
 
 List<String> tasks = [];
+List<Map<String, String>> userCredentials = [
+  {"email": "Student1@gmail.com", "password": "1234"},
+  {"email": "Student2@gmail.com", "password": "5678"},
+  {"email": "Student3@gmail.com", "password": "1122"},
+  {"email": "Teacher1@gmail.com", "password": "1234"},
+  {"email": "Teacher2@gmail.com", "password": "5678"},
+  {"email": "Teacher3@gmail.com", "password": "1122"},
+];
+List<Map<String, dynamic>> studentprofile = [
+  {
+    'Student Name': 'Student 1',
+    'Student Roll#': 1001,
+    'Semester': 'First',
+    'Feculty': 'BSCS',
+    'Student Email': 'Student1@gmail.com',
+    'Student Contect': '+92-335-1234567',
+    'Student Adress':
+        'plot no 123, Block x, Falana gali, dhimkana nagar, Karachi',
+    'Student Result': 'A Grade',
+    'Student Assignemnt': '5 Out of 6 submited',
+  },
+  {
+    'Student Name': 'Student 2',
+    'Student Roll#': 1002,
+    'Semester': 'Fourth',
+    'Feculty': 'BSCS',
+    'Student Email': 'Student2@gmail.com',
+    'Student Contect': '+92-335-1234567',
+    'Student Adress':
+        'plot no 121, Block x, Falana gali, dhimkana nagar, Karachi',
+    'Student Result': 'B Grade',
+    'Student Assignemnt': '4 Out of 6 submited',
+  },
+  {
+    'Student Name': 'Student 3',
+    'Student Roll#': 1003,
+    'Semester': 'Third',
+    'Feculty': 'BBA',
+    'Student Email': 'Student3@gmail.com',
+    'Student Contect': '+92-335-1234567',
+    'Student Adress':
+        'plot no 122, Block x, Falana gali, dhimkana nagar, Karachi',
+    'Student Result': 'C Grade',
+    'Student Assignemnt': '3 Out of 6 submited',
+  }
+];
 void main() {
   print('=========/\/\ WELLCOME TO ABC UNIVERSITY LMS /\/\=========');
 
@@ -10,6 +56,8 @@ void main() {
 
     if (command == 'Student') {
       StudentLogin();
+    } else if (command == 'Teacher') {
+      TeacherLogin();
     } else if (command == 'Exit') {
       break;
     } else {
@@ -31,12 +79,60 @@ String promptUser(String prompt) {
   return stdin.readLineSync()!;
 }
 
+TeacherLogin() {
+  bool Login = false;
+  while (!Login) {
+    String enteredEmail = promptUser("Enter your email:");
+    String enteredPassword = promptUser("Enter your password:");
+
+    if (userCredentials.any((credentials) =>
+        credentials["email"] == enteredEmail &&
+        credentials["password"] == enteredPassword)) {
+      Login = true;
+      print("User login successful.");
+      while (true) {
+        print(
+            'Select an option to go at(StudentProfile,AddStudent,RemoveStudent,Return)');
+        String tchoption = stdin.readLineSync()!;
+        if (tchoption == 'StudentProfile') {
+          print('Enter Roll no:');
+          var rollNo = int.parse(stdin.readLineSync()!);
+
+          for (var i = 0; i < studentprofile.length; i++) {
+            if (studentprofile[i]['Student Roll#'] == rollNo) {
+              print(studentprofile[i]);
+            }
+          }
+        } else if (tchoption == 'AddStudent') {
+          print('Enter Student Email Adress:');
+          var newStdEmail = stdin.readLineSync()!;
+          print('Enter Student Passwor:');
+          var newStdPass = stdin.readLineSync()!;
+          Map<String, String> newusercredentials = {
+            'email': newStdEmail,
+            'password': newStdPass,
+          };
+          userCredentials.add(newusercredentials);
+          print(userCredentials);
+          print('New Student Credential has been added');
+        } else if (tchoption == 'RemoveStudent') {
+          print('Enter Student Email Adress');
+          var removeStdEmail = stdin.readLineSync()!;
+          for (var i = 0; i < userCredentials.length; i++) {
+            if (removeStdEmail == userCredentials[i]["email"]) {
+              userCredentials.removeAt(i);
+            }
+          }
+          print(userCredentials);
+        } else if (tchoption == 'Return') {
+          break;
+        }
+      }
+    }
+  }
+}
+
 StudentLogin() {
-  List<Map<String, String>> userCredentials = [
-    {"email": "Student1@gmail.com", "password": "1234"},
-    {"email": "Student2@gmail.com", "password": "5678"},
-    {"email": "Student3@gmail.com", "password": "1122"},
-  ];
   bool Login = false;
 
   while (!Login) {
@@ -48,55 +144,31 @@ StudentLogin() {
         credentials["password"] == enteredPassword)) {
       Login = true;
       print("User login successful.");
-      print('Select an option to go at(StudentProfile,Result,Assignemnt,)');
-      String stdoption = stdin.readLineSync()!;
-      if (stdoption == 'StudentProfile') {
-        List<Map<String, dynamic>> studentprofile = [
-          {
-            'Student Name': 'Student 1',
-            'Student Roll#': 1001,
-            'Semester': 'First',
-            'Feculty': 'BSCS',
-            'Student Email': 'Student1@gmail.com',
-            'Student Contect': '+92-335-1234567',
-            'Student Adress':
-                'plot no 123, Block x, Falana gali, dhimkana nagar, Karachi',
-          },
-          {
-            'Student Name': 'Student 2',
-            'Student Roll#': 1002,
-            'Semester': 'Fourth',
-            'Feculty': 'BSCS',
-            'Student Email': 'Student2@gmail.com',
-            'Student Contect': '+92-335-1234567',
-            'Student Adress':
-                'plot no 121, Block x, Falana gali, dhimkana nagar, Karachi',
-          },
-          {
-            'Student Name': 'Student 3',
-            'Student Roll#': 1003,
-            'Semester': 'Third',
-            'Feculty': 'BBA',
-            'Student Email': 'Student3@gmail.com',
-            'Student Contect': '+92-335-1234567',
-            'Student Adress':
-                'plot no 122, Block x, Falana gali, dhimkana nagar, Karachi',
+      while (true) {
+        print(
+            'Select an option to go at(StudentProfile,Result,Assignemnt,Return)');
+        String stdoption = stdin.readLineSync()!;
+        if (stdoption == 'StudentProfile') {
+          for (var i = 0; i < studentprofile.length; i++) {
+            if (studentprofile[i]['Student Email'] == enteredEmail) {
+              print(studentprofile[i]);
+            }
           }
-        ];
-        if (studentprofile[0]['Student Email'] == enteredEmail) {
-          print(studentprofile[0]);
-          print('====================================');
-          print(stdoption);
-        } else if (studentprofile[1]['Student Email'] == enteredEmail) {
-          print(studentprofile[1]);
-          print('====================================');
-          print(stdoption);
-        } else if (studentprofile[2]['Student Email'] == enteredEmail) {
-          print(studentprofile[2]);
-          print('====================================');
-          print(stdoption);
+        } else if (stdoption == 'Assignemnt') {
+          for (var i = 0; i < studentprofile.length; i++) {
+            if (studentprofile[i]['Student Email'] == enteredEmail) {
+              print(studentprofile[i]['Student Assignemnt']);
+            }
+          }
+        } else if (stdoption == 'Result') {
+          for (var i = 0; i < studentprofile.length; i++) {
+            if (studentprofile[i]['Student Email'] == enteredEmail) {
+              print(studentprofile[i]['Student Result']);
+            }
+          }
+        } else if (stdoption == 'Return') {
+          break;
         }
-        ;
       }
     } else {
       print("Incorrect email or password. Please try again.");
